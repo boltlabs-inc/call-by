bdbc1d710ed422482c749e81c169f159545372b0
Kenny Foner <kwf@very.science>
2021-03-24 18:25:53 -0400

    Simplify names and add functionality: major version bump
    This renames:
    - `CallBy` => `By`
    - `CallingConvention` => `Convention`
    
    This adds:
    - The `Convert` trait, which can be used to convert between (some) pairs of calling conventions, as
      permitted by Rust's type system
    - The `As` trait, which can be used to generalize `Into`, `AsRef`, and `AsMut` in a
      convention-polymorphic context

--- src/lib.rs
+++ src/lib.rs
@@ -1,61 +1,63 @@
-//! To parameterize a function by calling convention, we can specify that it takes some `T:
-//! CallBy<'a, Convention>`, and say that its input is of type `<T as CallBy<'a,
-//! Convention>>::Type`. This is essentially a defunctionalization of Rust's reference operators.
-//!
-//! This trick can be used to permit the *implementor* of a trait to pick the calling convention for
-//! a value passed into (or out of) a function defined in that trait, rather than this being
-//! hardcoded in the trait definition.
-//!
-//! ## Examples
-//!
-//! For instance, say we wanted to define an abstraction for channels that can send values. Imagine,
-//! however, that some channels might need to take ownership of the values they send, while others
-//! might serialize values given only a reference to that value. In order to unify these two notions
-//! into one trait, we can parameterize over the calling convention for the input value:
-//!
-//! ```rust
-//! use call_by::{CallBy, CallingConvention};
-//!
-//! trait Sender<'a, T>
-//! where
-//!     T: CallBy<'a, Self::Convention>,
-//! {
-//!     type Convention: CallingConvention;
-//!     fn send(&self, value: <T as CallBy<'a, Self::Convention>>::Type);
-//! }
-//! ```
-//!
-//! Implementers of the `Sender` trait can choose whether the associated type `Convention` should be
-//! `Val`, `Ref`, or `Mut`, which toggles the result of `<T as CallBy<'a, Self::Convention>>::Type`
-//! between `T`, `&'a T`, and `&'a mut T`, respectively. Meanwhile, callers of the `send` method on
-//! concretely known types don't need to specify the calling convention; the type-level function
-//! determines what type they need to pass as the argument to `send`, and type errors are reported
-//! in reference to that concrete type if it is known at the call site.
-//! <!-- snip -->
+/*!
+To parameterize a function by calling convention, we can specify that it takes some `T:
+By<'a, Convention>`, and say that its input is of type `<T as By<'a,
+Convention>>::Type`. This is essentially a defunctionalization of Rust's reference operators.
+
+This trick can be used to permit the *implementor* of a trait to pick the calling convention for
+a value passed into (or out of) a function defined in that trait, rather than this being
+hardcoded in the trait definition.
+
+## Examples
+
+For instance, say we wanted to define an abstraction for channels that can send values. Imagine,
+however, that some channels might need to take ownership of the values they send, while others
+might serialize values given only a reference to that value. In order to unify these two notions
+into one trait, we can parameterize over the calling convention for the input value:
+
+```rust
+use call_by::{By, Convention};
+
+trait Sender<'a, T>
+where
+    T: By<'a, Self::Convention>,
+{
+    type Convention: Convention;
+    fn send(&self, value: <T as By<'a, Self::Convention>>::Type);
+}
+```
+
+Implementers of the `Sender` trait can choose whether the associated type `Convention` should be
+`Val`, `Ref`, or `Mut`, which toggles the result of `<T as By<'a, Self::Convention>>::Type`
+between `T`, `&'a T`, and `&'a mut T`, respectively. Meanwhile, callers of the `send` method on
+concretely known types don't need to specify the calling convention; the type-level function
+determines what type they need to pass as the argument to `send`, and type errors are reported
+in reference to that concrete type if it is known at the call site.
+<!-- snip -->
+*/
 
 /// There are three fundamental ways to pass a `T` as input or return a `T` as output: by [`Val`]ue,
 /// by shared immutable [`Ref`]erence, and by unique [`Mut`]able reference.
 ///
 /// This is a sealed trait, implemented for all three of these conventions.
-pub trait CallingConvention: sealed::CallingConvention {
+pub trait Convention: sealed::Convention + Sized {
     const TOKEN: Self;
 }
 
-impl CallingConvention for Val {
+impl Convention for Val {
     const TOKEN: Self = Val;
 }
 
-impl CallingConvention for Ref {
+impl Convention for Ref {
     const TOKEN: Self = Ref;
 }
 
-impl CallingConvention for Mut {
+impl Convention for Mut {
     const TOKEN: Self = Mut;
 }
 
-/// To get the type of `T` via calling convention `Convention`, write `<T as CallBy<'a,
+/// To get the type of `T` via calling convention `Convention`, write `<T as By<'a,
 /// Convention>>::Type`.
-pub trait CallBy<'a, Convention: CallingConvention> {
+pub trait By<'a, C: Convention> {
     /// The type of `Self` when called by `Convention`.
     type Type;
 }
@@ -64,7 +66,7 @@ pub trait CallBy<'a, Convention: CallingConvention> {
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, Default)]
 pub struct Val;
 
-impl<'a, T> CallBy<'a, Val> for T {
+impl<'a, T> By<'a, Val> for T {
     type Type = T;
 }
 
@@ -72,7 +74,7 @@ impl<'a, T> CallBy<'a, Val> for T {
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, Default)]
 pub struct Ref;
 
-impl<'a, T: 'a> CallBy<'a, Ref> for T {
+impl<'a, T: 'a> By<'a, Ref> for T {
     type Type = &'a T;
 }
 
@@ -81,15 +83,143 @@ impl<'a, T: 'a> CallBy<'a, Ref> for T {
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, Default)]
 pub struct Mut;
 
-impl<'a, T: 'a> CallBy<'a, Mut> for T {
+impl<'a, T: 'a> By<'a, Mut> for T {
     type Type = &'a mut T;
+}
+
+/// Convert between different calling conventions.
+///
+/// Only some conversions are sensible in Rust, due to the ownership system. These are the valid
+/// conversions, with the constraints on the underlying type `T` noted:
+///
+/// | Can I convert... | ... to [`Val`] (`T`)  | ... to [`Ref`] (`&'a T`) | ... to [`Mut`] (`&'a mut T`) |
+/// | :--------------------------------- | :------------------ | :------ | :------ |
+/// | **from [`Val`] (`T`) ...**         | (valid for all `T`) | ❌*     | ❌*     |
+/// | **from [`Ref`] (`&'a T`) ...**     | `T: 'a +` [`Clone`] | `T: 'a` | ❌**    |
+/// | **from [`Mut`] (`&'a mut T`) ...** | `T: 'a +` [`Clone`] | `T: 'a` | `T: 'a` |
+///
+/// > \* Impossible because references can't outlive the data they borrow.
+/// >
+/// > \** Impossible because potentially-aliased data can't be mutably referenced.
+pub trait Convert<'a, From: Convention, To: Convention>
+where
+    Self: By<'a, To> + By<'a, From>,
+{
+    /// Convert from one calling convention to another.
+    ///
+    /// Because of the generic parameters on the trait, this often requires rather explicit type
+    /// annotations.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use call_by::*;
+    ///
+    /// let a: u8 = <u8 as Convert<Val, Val>>::convert(1);
+    /// let b: u8 = <u8 as Convert<Ref, Val>>::convert(&2);      // implicit clone
+    /// let c: u8 = <u8 as Convert<Mut, Val>>::convert(&mut 3);  // implicit clone
+    ///
+    /// let d: &u8 = <u8 as Convert<Ref, Ref>>::convert(&4);
+    /// let e: &u8 = <u8 as Convert<Mut, Ref>>::convert(&mut 5);
+    ///
+    /// let b: &mut u8 = <u8 as Convert<Mut, Mut>>::convert(&mut 6);
+    /// ```
+    #[allow(clippy::wrong_self_convention)]
+    fn convert(from: <Self as By<'a, From>>::Type) -> <Self as By<'a, To>>::Type;
+}
+
+impl<'a, T> Convert<'a, Val, Val> for T {
+    fn convert(from: T) -> T {
+        from
+    }
+}
+
+impl<'a, T: 'a + Clone> Convert<'a, Ref, Val> for T {
+    fn convert(from: &T) -> T {
+        from.clone()
+    }
+}
+
+impl<'a, T: 'a + Clone> Convert<'a, Mut, Val> for T {
+    fn convert(from: &mut T) -> T {
+        from.clone()
+    }
+}
+
+impl<'a, T: 'a> Convert<'a, Ref, Ref> for T {
+    fn convert(from: &T) -> &T {
+        from
+    }
+}
+
+impl<'a, T: 'a> Convert<'a, Mut, Ref> for T {
+    fn convert(from: &mut T) -> &T {
+        &*from
+    }
+}
+
+impl<'a, T: 'a> Convert<'a, Mut, Mut> for T {
+    fn convert(from: &mut T) -> &mut T {
+        from
+    }
+}
+
+/// The generalization of [`Into`], [`AsRef`], and [`AsMut`]: in a calling-convention polymorphic
+/// context, this trait allows you to invoke the appropriate conversion method depending on the
+/// applicable calling convention.
+///
+/// # Examples
+///
+/// ```
+/// use call_by::*;
+///
+/// fn do_something<'a, T, S, C>(input: <S as By<'a, C>>::Type)
+/// where
+///     T: By<'a, C>,
+///     S: By<'a, C> + As<'a, C, T>,
+///     C: Convention,
+/// {
+///     let t: <T as By<'a, C>>::Type = S::as_convention(input);
+///     // ... do something with `t` ...
+/// }
+/// ```
+pub trait As<'a, C: Convention, T: By<'a, C>>: By<'a, C> {
+    #[allow(clippy::wrong_self_convention)]
+    fn as_convention(this: <Self as By<'a, C>>::Type) -> <T as By<'a, C>>::Type;
+}
+
+impl<'a, T, S> As<'a, Val, T> for S
+where
+    S: Into<T>,
+{
+    fn as_convention(this: S) -> T {
+        this.into()
+    }
+}
+
+impl<'a, T: 'a, S: 'a> As<'a, Ref, T> for S
+where
+    S: AsRef<T>,
+{
+    fn as_convention(this: &S) -> &T {
+        this.as_ref()
+    }
+}
+
+impl<'a, T: 'a, S: 'a> As<'a, Mut, T> for S
+where
+    S: AsMut<T>,
+{
+    fn as_convention(this: &mut S) -> &mut T {
+        this.as_mut()
+    }
 }
 
 mod sealed {
     use super::*;
 
-    pub trait CallingConvention {}
-    impl CallingConvention for Val {}
-    impl CallingConvention for Ref {}
-    impl CallingConvention for Mut {}
+    pub trait Convention {}
+    impl Convention for Val {}
+    impl Convention for Ref {}
+    impl Convention for Mut {}
 }
